9cc4f2a1f4c50b1854fe3066c8b2bbbbc00347ae
Thomas Kao <tkao@sumologic.com>
2020-05-14 22:44:17 -0700

    SUMO-128790: autogenerate test values from examples/patterns/defaults

---
+++ src/main/scala/com/sumologic/terraform_generator/writer/AcceptanceTestGeneratorHelper.scala
@@ -0,0 +1,72 @@
+package com.sumologic.terraform_generator.writer
+
+import com.sumologic.terraform_generator.StringHelper
+import com.sumologic.terraform_generator.objects.{ScalaSwaggerObject, ScalaSwaggerObjectArray}
+import nl.flotsam.xeger.Xeger
+
+import scala.util.Random
+
+trait AcceptanceTestGeneratorHelper extends StringHelper {
+  def getTestValue(prop: ScalaSwaggerObject, isUpdate: Boolean = false): String = {
+    prop.getType().name match {
+      case "bool" =>
+        if (prop.getDefault().isDefined) {
+          prop.getDefault().get.toString
+        } else {
+          "false"
+        }
+      case "int64" | "int32" =>
+        if (prop.getDefault().isDefined) {
+          prop.getDefault().get.toString
+        } else if (prop.getExample().nonEmpty) {
+          prop.getExample()
+        } else {
+          "0"
+        }
+      case "[]string" =>
+        if (prop.getDefault().isDefined) {
+          val default = prop.getDefault().get.asInstanceOf[List[String]]
+          s"""[]string{${default.map {
+            item => s"""$item"""
+          }.mkString(", ")}}"""
+        } else if (prop.getExample().nonEmpty) {
+          val items = prop.getExample().replace("[", "").replace("]", "").split(",")
+          s"""[]string{${items.map {
+            item => s"""$item"""
+          }.mkString(", ")}}"""
+        } else if (prop.asInstanceOf[ScalaSwaggerObjectArray].getPattern().nonEmpty) {
+          val generator = new Xeger(prop.getPattern())
+          val generatedString = generator.generate()
+          s"""[]string{"$generatedString"}"""
+        } else {
+          val r = new Random
+          val sb = new StringBuilder
+          for (i <- 1 to 10) {
+            sb.append(r.nextPrintableChar)
+          }
+          s"""[]string{"${sb.toString()}"}"""
+        }
+      case "string" =>
+        //NOTE: Making an assumption that there will always be at least one string field
+        val update = if (isUpdate) {"Update"} else {""}
+        if (prop.getDefault().isDefined) {
+          s""""${prop.getDefault().get.toString.replace(""""""", """\"""")}$update""""
+        } else if (prop.getExample().nonEmpty) {
+          s""""${prop.getExample().toString.replace(""""""", """\"""")}$update""""
+        } else if (prop.getPattern().nonEmpty) {
+          val generator = new Xeger(prop.getPattern())
+          generator.generate()
+          s""""${generator.generate().replace(""""""", """\"""")}""""
+        } else {
+          val r = new Random
+          val sb = new StringBuilder
+          for (i <- 1 to 10) {
+            sb.append(r.nextPrintableChar)
+          }
+          s"""${sb.toString.replace(""""""", """\"""")}"""
+        }
+      case _ =>
+        throw new RuntimeException("Trying to generate test values for an unsupported type.")
+    }
+  }
+}
