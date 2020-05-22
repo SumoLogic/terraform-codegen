6d231cf95c066b7bcad682a9e85982b58f77cecc
Thomas Kao <tkao@sumologic.com>
2020-05-21 22:41:31 -0700

    SUMO-0: try new pom file

--- bin/run.sh
+++ bin/run.sh
@@ -7,7 +7,7 @@ if [ $# -eq 0 ]
 	echo "Must supply input yaml file"
 elif [ $# -eq 1 ]
     then
-	./mvnw compile exec:java -Dexec.mainClass="com.sumologic.terraform_generator.TerraformGenerator" -Dexec.args="$JAVA_PROGRAM_ARGS"
+	./mvnw compile exec:java -Dexec.mainClass="com.sumologic.terraform_generator.TerraformGenerator" -Dexec.args="$JAVA_PROGRAM_ARGS" -Duser.home="SwaggerToTerraformGenerator"
 else
-    ./mvnw compile exec:java -Dexec.mainClass="com.sumologic.terraform_generator.TerraformGenerator" -Dexec.args="$JAVA_PROGRAM_ARGS"
+    ./mvnw compile exec:java -Dexec.mainClass="com.sumologic.terraform_generator.TerraformGenerator" -Dexec.args="$JAVA_PROGRAM_ARGS" -Duser.home="SwaggerToTerraformGenerator"
 fi
